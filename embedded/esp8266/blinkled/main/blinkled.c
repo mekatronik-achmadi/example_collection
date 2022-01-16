bf420eb28407bd0f8787b6f5dddd0d08ccdbe167
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2022-01-16 18:44:37 +0700

    esp8266 example

---
+++ embedded/esp8266/blinkled/main/blinkled.c
@@ -0,0 +1,27 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/portmacro.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO  16
+#define BLINK_DELAY 200
+
+void led_task(void *pvParameter){
+    while (1) {
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
+
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main(void){
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    xTaskCreate(&led_task, "led blink", 1024, NULL, 5, NULL);
+
+    while(1){
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
