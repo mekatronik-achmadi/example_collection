26965035f1476fa18fb3f95940afc12462f82667
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2022-07-20 18:38:53 +0700

    ESP32 IDF Arduino Example

---
+++ embedded/esp32/blink_oled/idf/main/blinkled.c
@@ -0,0 +1,19 @@
+#include "esp32/rom/ets_sys.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+#define BLINK_GPIO  2
+#define BLINK_DELAY 200
+
+void app_main(void){
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    while(1){
+        gpio_set_level(BLINK_GPIO, 0);
+        ets_delay_us(500000);
+
+        gpio_set_level(BLINK_GPIO, 1);
+        ets_delay_us(500000);
+    }
+}
