1f3b89cc99b8b6d117077d457b14fe6acd870c7d
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2022-07-21 08:29:31 +0700

    ESP32 Arduino OLED Example

---
+++ embedded/esp32/blink_oled/arduino/myoledlcd/examples/text_drawing/text_drawing.ino
@@ -0,0 +1,18 @@
+#include <myoledlcd.h>
+
+#define BLINK_GPIO  GPIO_NUM_2
+
+void setup(void){
+  gpio_reset_pin(BLINK_GPIO);
+  gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+  my_oledInit();
+  my_olcdtest_text();
+}
+
+void loop(void){
+    gpio_set_level(BLINK_GPIO, 0);
+    ets_delay_us(500000);
+
+    gpio_set_level(BLINK_GPIO, 1);
+    ets_delay_us(500000);
+}
