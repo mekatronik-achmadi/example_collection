b0172986b1c962df4bb1393412572047cc460952
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2023-10-30 13:06:24 +0700

    Lora Sender Arduino

--- embedded/atmega/lora/arduino_sender/arduino_sender.ino
+++ embedded/atmega/lora/arduino_sender/arduino_sender.ino
@@ -8,15 +8,26 @@ void setup() {
   while (!Serial);  
   Serial.println("LoRa Sender");
 
-//  if (!LoRa.begin(433E6)) { // or 915E6, the MHz speed of yout module
-//    Serial.println("Starting LoRa failed!");
-//    while (1);
-//  }
+  if (!LoRa.begin(433E6)) { // or 915E6, the MHz speed of yout module
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  Serial.println("LoRa Initiated");
 }
 
 void loop() {
+  int val = 100;
+
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);  
+  delay(1000);
+
+  LoRa.beginPacket();
+  LoRa.print(val);
+  LoRa.endPacket();
+
   digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(1000);
+
+  Serial.println("LoRa Data Send");
 }
